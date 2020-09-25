2dd07e10252879dac8d235ba9bf28ff25169d48e
AnkithGujar <ankith.gujar05@gmail.com>
2020-09-25 10:32:04 -0400

    Added object detection using yolo v2 tiny

--- drive.py
+++ drive.py
@@ -4,13 +4,62 @@ import time
 import utils
 import random
 import numpy as np
-from getkeys import key_check
 from statistics import mode,mean
-from grabscreen import grab_screen
 from keras.models import load_model
 from darkflow.net.build import TFNet
 from collections import deque, Counter
 from vjoy import vJoy, ultimate_release
+import win32gui, win32ui, win32con, win32api
+
+
+# list of keys that will be checked
+keyList = ["\b"]
+for char in "ABCDEFGHIJKLMNOPQRSTUVWXYZ 123456789,.'£$/\\":
+    keyList.append(char)
+
+# function the collect the keyboard inputs
+def key_check():
+    keys = []
+    for key in keyList:
+        if win32api.GetAsyncKeyState(ord(key)):
+            keys.append(key)
+    return keys
+
+
+# function to grab a screenshot 
+# at the mentioned coordinates
+def grab_screen(region=None):
+
+    hwin = win32gui.GetDesktopWindow()
+
+    if region:
+            left,top,x2,y2 = region
+            width = x2 - left + 1
+            height = y2 - top + 1
+    else:
+        width = win32api.GetSystemMetrics(win32con.SM_CXVIRTUALSCREEN)
+        height = win32api.GetSystemMetrics(win32con.SM_CYVIRTUALSCREEN)
+        left = win32api.GetSystemMetrics(win32con.SM_XVIRTUALSCREEN)
+        top = win32api.GetSystemMetrics(win32con.SM_YVIRTUALSCREEN)
+
+    hwindc = win32gui.GetWindowDC(hwin)
+    srcdc = win32ui.CreateDCFromHandle(hwindc)
+    memdc = srcdc.CreateCompatibleDC()
+    bmp = win32ui.CreateBitmap()
+    bmp.CreateCompatibleBitmap(srcdc, width, height)
+    memdc.SelectObject(bmp)
+    memdc.BitBlt((0, 0), (width, height), srcdc, (left, top), win32con.SRCCOPY)
+    
+    signedIntsArray = bmp.GetBitmapBits(True)
+    img = np.fromstring(signedIntsArray, dtype='uint8')
+    img.shape = (height,width,4)
+
+    srcdc.DeleteDC()
+    memdc.DeleteDC()
+    win32gui.ReleaseDC(hwin, hwindc)
+    win32gui.DeleteObject(bmp.GetHandle())
+
+    return cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
 
 
 def main():
